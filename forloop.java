bff5cefee2387f1984a22bf77f8c73af73496fcd
Dharmendra Kumar <dksharma8115@gmail.com>
2020-07-29 08:28:18 +0530

    upadted with new feature

--- forloop.java
+++ forloop.java
@@ -1,2 +1,10 @@
+class forloop
+{
+public static void main(String[] args)
+{
 for(int i=0;i<10;i++){
 System.out.println("Hello");
+System.out.println("done with for loops");
+System.out.println("wait what about while loop");
+}
+}
